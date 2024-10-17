fbc767a67e482edac58e58a1d6b1e8087bdb45cd
keshan200 <sachithakeshan175@gmail.com>
2024-10-17 19:49:39 +0530

    updated title and logo

---
+++ assets/js/pre-loader.js
@@ -0,0 +1,8 @@
+// JavaScript to hide the preloader and show the content
+window.addEventListener('load', () => {
+    // Delay to simulate loading time
+    setTimeout(() => {
+        document.querySelector('.preloader').style.display = 'none'; // Hide preloader
+        document.querySelector('.main-content').style.display = 'block'; // Show main content
+    }, 3000); // Set delay in milliseconds (3 seconds)
+});
